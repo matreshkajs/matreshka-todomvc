e06c07465cfe5f59c33845ec47e1284292d96506
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-19 22:42:23 +0300

    use es2015 and Matreshka.js 2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
 // This is documentation for the implementation of the [TodoMVC](http://todomvc.com)  using the [Matreshka.js](http://ru.matreshka.io) framework as a basis. The working version is located [on this page](../).
 
 // As it is required by the specification of TodoMVC, assign the codes of keys to the corresponding variables.
-var ENTER_KEY = 13;
-var ESC_KEY = 27;
+const ENTER_KEY = 13;
+const ESC_KEY = 27;
 
-(function(window) {
-	'use strict';
-	// Initialize the application
-	window.app = new Todos;
-	// See [todos.js](todos.html) and [todo.js](todo.html)
-})(window);
+// Initialize the application
+window.app = new Todos();
+// See [todos.js](todos.html) and [todo.js](todo.html)
